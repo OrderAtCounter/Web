0326551cd88e4a8c9ef003a716dd8d98dde7d904
Chris Amavisca <chris.amavisca@gmail.com>
2013-09-12 11:21:17 -0400

    Revert "SessionID is _id"
    This reverts commit 8ff87f2142bd726c9248e4e93159cfeaa3592d30.

--- routes/index.js
+++ routes/index.js
@@ -80,7 +80,7 @@ exports.login = function(req, res) {
 exports.logout = function(req, res) {
   var username = req.body['username'];
   var sessionId = req.body['sessionId'];
-  Session.findOne({_id: sessionId, username: username}, function(err, session) {
+  Session.findOne({sessionId: sessionId, username: username}, function(err, session) {
     if(err) {
       res.send(500);
     }
