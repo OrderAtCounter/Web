b34969f77efdbe727e27442c8b8444d2cc86ae09
Chris Amavisca <chris.amavisca@gmail.com>
2013-11-11 11:03:42 -0500

    Login page styling

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,3 +1,13 @@
-$('#loginButton').click(function() {
-  $('#loginForm').submit();
+$('.loginButton').click(function() {
+    $('.loginForm').submit();
+});
+
+$('#inputEmail').focus();
+
+$('.loginForm').keypress(function(e) {
+  if(e.which) {
+    if(e.which === 13) {
+      $('.loginButton').click();
+    }
+  }
 });
