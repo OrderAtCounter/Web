daead2ca4e441a2d8fb2ae2690e7e9a6fcb84567
Chris Amavisca <chris.amavisca@gmail.com>
2013-11-12 16:46:58 -0500

    Signup looks hella nice

--- public/javascripts/payment.js
+++ public/javascripts/payment.js
@@ -1,6 +1,6 @@
 var selectedPayment;
-var initColors = ['#4C4A49','#71C2B3', '#EF493A'];
-var afterColors = ['#000000','#68AFA1', '#D33C35'];
+// var initColors = ['#4C4A49','#71C2B3', '#EF493A'];
+// var afterColors = ['#000000','#68AFA1', '#D33C35'];
 
 
 $('.choosePlan').hover(function() {
@@ -22,16 +22,17 @@ $('.choosePlan').click(function() {
   else {
     $('.choosePlan').text('Select plan');
     $('.choosePlan').parent().css({'background-color': '#F7F7F7'});
+    $('.choosePlan').hover(function() {
+      $(this).parent().css({'background-color': '#FCFCFC'});
+    }, function() {
+      $(this).parent().css({'background-color': '#F7F7F5'});
+    });
     $(this).text('Selected');
     $(this).parent().css({'background-color': '#FCFCFC'});
     $(this).unbind('mouseenter mouseleave');
   }
 });
 
-$('.choosePlan').hover(function() {
-  $(this).parent().css({'background-color': '#FCFCFC'});
-}, function() {
-  $(this).parent().css({'background-color': '#F7F7F5'});
-});
-
 $('#cardNumberInput').payment('formatCardNumber');
+$('#cvcInput').payment('formatCardCVC');
+$('#expirationInput').payment('formatCardExpiry');
