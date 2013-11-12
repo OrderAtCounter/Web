89ba22845bd1e839bc622914da3f1298a6a90d76
Chris Amavisca <chris.amavisca@gmail.com>
2013-11-12 15:45:30 -0500

    lots of payment styling

---
+++ public/javascripts/payment.js
@@ -0,0 +1,37 @@
+var selectedPayment;
+var initColors = ['#4C4A49','#71C2B3', '#EF493A'];
+var afterColors = ['#000000','#68AFA1', '#D33C35'];
+
+
+$('.choosePlan').hover(function() {
+  $(this).parent().css({'background-color': '#FCFCFC'});
+}, function() {
+  $(this).parent().css({'background-color': '#F7F7F5'});
+});
+
+$('.choosePlan').click(function() {
+  if($(this).text() === 'Selected') {
+    $(this).text('Select plan');
+    $(this).parent().css({'background-color': '#F7F7F7'});
+    $('.choosePlan').hover(function() {
+      $(this).parent().css({'background-color': '#FCFCFC'});
+    }, function() {
+      $(this).parent().css({'background-color': '#F7F7F5'});
+    });
+  }
+  else {
+    $('.choosePlan').text('Select plan');
+    $('.choosePlan').parent().css({'background-color': '#F7F7F7'});
+    $(this).text('Selected');
+    $(this).parent().css({'background-color': '#FCFCFC'});
+    $(this).unbind('mouseenter mouseleave');
+  }
+});
+
+$('.choosePlan').hover(function() {
+  $(this).parent().css({'background-color': '#FCFCFC'});
+}, function() {
+  $(this).parent().css({'background-color': '#F7F7F5'});
+});
+
+$('#cardNumberInput').payment('formatCardNumber');
